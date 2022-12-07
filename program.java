2cc9b62bc1239671a64abffba5fdf4ae8c94609f
BananaFlamber <KorolevSteel@inbox.ru>
2022-12-07 21:02:51 +0300

    first task - done

---
+++ program.java
@@ -0,0 +1,12 @@
+public class program {
+    public static void main(String[] args) {
+        int numbA = 15;
+        int numbB = 5;
+        System.out.println(checkMethod(numbA, numbB));
+    }
+    public static boolean checkMethod(int numbA,int numbB) {
+        return numbA + numbB >= 10 && numbA + numbB <= 20;
+    }
+    public static
+}
+
