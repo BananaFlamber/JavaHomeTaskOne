e987849f0b418f9d889829a4217d3bf2db08edef
BananaFlamber <KorolevSteel@inbox.ru>
2022-12-07 21:12:09 +0300

    second task - done

--- program.java
+++ program.java
@@ -1,12 +1,21 @@
 public class program {
     public static void main(String[] args) {
-        int numbA = 15;
+        int numbA = -15;
         int numbB = 5;
-        System.out.println(checkMethod(numbA, numbB));
+//        System.out.println(checkMethod(numbA, numbB));
+//        checkNumb(numbA);
     }
     public static boolean checkMethod(int numbA,int numbB) {
         return numbA + numbB >= 10 && numbA + numbB <= 20;
     }
-    public static
+    public static void checkNumb(int numbA) {
+        if (numbA > 0) {
+            System.out.println("Число положительное");
+        }
+        else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
 }
 
